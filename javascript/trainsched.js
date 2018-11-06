281109d4f1384929c50be380880a2179fb4c93c7
singmokjai <lijason001@gmail.com>
2018-11-06 12:26:14 -0500

    firebase config

---
+++ javascript/trainsched.js
@@ -0,0 +1,24 @@
+// Fire base config
+<script src="https://www.gstatic.com/firebasejs/5.5.7/firebase.js"></script>
+<script>
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEgeZks27WRLzRkEVpYURR9xdfVUy8Xn8",
+    authDomain: "fire-train-26bd0.firebaseapp.com",
+    databaseURL: "https://fire-train-26bd0.firebaseio.com",
+    projectId: "fire-train-26bd0",
+    storageBucket: "fire-train-26bd0.appspot.com",
+    messagingSenderId: "450750880238"
+};
+firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+</script>
